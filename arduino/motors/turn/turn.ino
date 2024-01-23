0e6766b70ed40855d596b6869ef7db69c670b55a
Eric Foy <foy.8@wright.edu>
2024-01-23 18:45:56 -0500

    motors turn left and right, in place and moving turn

---
+++ arduino/motors/turn/turn.ino
@@ -0,0 +1,63 @@
+int enablePin1 = 6;
+int L293D_1A = 5;
+int L293D_2A = 4;
+
+int enablePin2 = 10;
+int L293D_3A = 9;
+int L293D_4A = 8;
+
+void setup()
+{
+  pinMode(enablePin1, OUTPUT);
+  pinMode(L293D_1A, OUTPUT);
+  pinMode(L293D_2A, OUTPUT);
+
+  pinMode(enablePin2, OUTPUT);
+  pinMode(L293D_3A, OUTPUT);
+  pinMode(L293D_4A, OUTPUT);
+}
+
+void loop() 
+{
+  // motor 1 on left
+
+  // turn in place (right)
+  digitalWrite(L293D_1A, LOW);
+  digitalWrite(L293D_2A, HIGH);
+
+  digitalWrite(L293D_3A, LOW);
+  digitalWrite(L293D_4A, HIGH);
+
+  analogWrite(enablePin1, 200);
+  analogWrite(enablePin2, 200);
+
+  // turn in place (left)
+  digitalWrite(L293D_1A, HIGH);
+  digitalWrite(L293D_2A, LOW);
+
+  digitalWrite(L293D_3A, HIGH);
+  digitalWrite(L293D_4A, LOW);
+
+  analogWrite(enablePin1, 200);
+  analogWrite(enablePin2, 200);
+
+  // moving turn (left)
+  digitalWrite(L293D_1A, HIGH);
+  digitalWrite(L293D_2A, LOW);
+
+  digitalWrite(L293D_3A, LOW);
+  digitalWrite(L293D_4A, HIGH);
+
+  analogWrite(enablePin1, 240);
+  analogWrite(enablePin2, 160);
+
+  // moving turn (right)
+  digitalWrite(L293D_1A, HIGH);
+  digitalWrite(L293D_2A, LOW);
+
+  digitalWrite(L293D_3A, LOW);
+  digitalWrite(L293D_4A, HIGH);
+
+  analogWrite(enablePin1, 160);
+  analogWrite(enablePin2, 240);
+}
