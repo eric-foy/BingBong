80313a2ba5630ff10c7f3243761ff60a2f36c313
Eric Foy <foy.8@wright.edu>
2024-01-25 08:30:53 -0500

    print statements for turn

--- arduino/motors/turn/turn.ino
+++ arduino/motors/turn/turn.ino
@@ -8,6 +8,7 @@ int L293D_4A = 8;
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(enablePin1, OUTPUT);
   pinMode(L293D_1A, OUTPUT);
   pinMode(L293D_2A, OUTPUT);
@@ -21,6 +22,7 @@ void loop()
 {
   // motor 1 on left
 
+  Serial.println("Turn in place (right)");
   // turn in place (right)
   digitalWrite(L293D_1A, LOW);
   digitalWrite(L293D_2A, HIGH);
@@ -31,6 +33,9 @@ void loop()
   analogWrite(enablePin1, 200);
   analogWrite(enablePin2, 200);
 
+  delay(8000);
+
+  Serial.println("Turn in place (left)");
   // turn in place (left)
   digitalWrite(L293D_1A, HIGH);
   digitalWrite(L293D_2A, LOW);
@@ -41,6 +46,9 @@ void loop()
   analogWrite(enablePin1, 200);
   analogWrite(enablePin2, 200);
 
+  delay(8000);
+
+  Serial.println("Moving turn (left)");
   // moving turn (left)
   digitalWrite(L293D_1A, HIGH);
   digitalWrite(L293D_2A, LOW);
@@ -51,6 +59,7 @@ void loop()
   analogWrite(enablePin1, 240);
   analogWrite(enablePin2, 160);
 
+  Serial.println("Moving turn (right)");
   // moving turn (right)
   digitalWrite(L293D_1A, HIGH);
   digitalWrite(L293D_2A, LOW);
