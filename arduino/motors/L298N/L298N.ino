a1ea76f0f7bdfd454f569a3f601100fefd352f31
Eric Foy <foy.8@wright.edu>
2024-02-21 20:23:38 -0500

    turn left and right

--- arduino/motors/L298N/L298N.ino
+++ arduino/motors/L298N/L298N.ino
@@ -2,18 +2,18 @@
 
 
 // Right Motors
-//int enablePin1 = 3;
+int enablePin1 = 3;
 int L298N_1A = 2;
 int L298N_2A = 4;
-//int enablePin2 = 6;
+int enablePin2 = 6;
 int L298N_3A = 5;
 int L298N_4A = 7;
 
 // Left Motors
-//int enablePin1 = 9;
+int enablePin3 = 9;
 int L298N_2_1A = 8;
 int L298N_2_2A = 10;
-//int enablePin2 = 11;
+int enablePin4 = 11;
 int L298N_2_3A = 12;
 int L298N_2_4A = 13;
 
@@ -25,11 +25,11 @@ void setup()
   // Right Motors
   // ---------------------------------------------------
 
-  //pinMode(enablePin1, OUTPUT);
+  pinMode(enablePin1, OUTPUT);
   pinMode(L298N_1A, OUTPUT);
   pinMode(L298N_2A, OUTPUT);
 
-  //pinMode(enablePin2, OUTPUT);
+  pinMode(enablePin2, OUTPUT);
   pinMode(L298N_3A, OUTPUT);
   pinMode(L298N_4A, OUTPUT);
 
@@ -37,10 +37,11 @@ void setup()
   // Left Motors
   // ---------------------------------------------------
 
+  pinMode(enablePin3, OUTPUT);
   pinMode(L298N_2_1A, OUTPUT);
   pinMode(L298N_2_2A, OUTPUT);
 
-  //pinMode(enablePin2, OUTPUT);
+  pinMode(enablePin4, OUTPUT);
   pinMode(L298N_2_3A, OUTPUT);
   pinMode(L298N_2_4A, OUTPUT);
 
@@ -48,41 +49,28 @@ void setup()
   // Main
   // ---------------------------------------------------
 
-  forward(500);
-  spinLeft(500);
-  forward(500);
+  forward(600, 200);
+  spinLeft(600, 200);
+  forward(300, 200);
   stop();
 }
 
 void stop()
 {
-  // ---------------------------------------------------
-  // Right Motors
-  // ---------------------------------------------------
-
-  // Counter Clockwise left motor
-  digitalWrite(L298N_1A, LOW);
-  digitalWrite(L298N_2A, LOW);
-
-  // Clockwise right motor
-  digitalWrite(L298N_3A, LOW);
-  digitalWrite(L298N_4A, LOW);
-
-  // ---------------------------------------------------
-  // Left Motors
-  // ---------------------------------------------------
-
-  // Counter Clockwise left motor
-  digitalWrite(L298N_2_1A, LOW);
-  digitalWrite(L298N_2_2A, LOW);
-
-  // Clockwise right motor
-  digitalWrite(L298N_2_3A, LOW);
-  digitalWrite(L298N_2_4A, LOW);
+  analogWrite(enablePin1, 0);
+  analogWrite(enablePin2, 0);
+  analogWrite(enablePin3, 0);
+  analogWrite(enablePin4, 0);
 }
 
-void forward(int delayMilli)
+// duration in milliseconds, speed between 0 and 255
+void forward(int duration, int speed)
 {
+  analogWrite(enablePin1, speed);
+  analogWrite(enablePin2, speed);
+  analogWrite(enablePin3, speed);
+  analogWrite(enablePin4, speed);
+
   // ---------------------------------------------------
   // Right Motors
   // ---------------------------------------------------
@@ -107,11 +95,17 @@ void forward(int delayMilli)
   digitalWrite(L298N_2_3A, HIGH);
   digitalWrite(L298N_2_4A, LOW);
 
-  delay(delayMilli);
+  delay(duration);
 }
 
-void backward(int delayMilli)
+// duration in milliseconds, speed between 0 and 255
+void backward(int duration, int speed)
 {
+  analogWrite(enablePin1, speed);
+  analogWrite(enablePin2, speed);
+  analogWrite(enablePin3, speed);
+  analogWrite(enablePin4, speed);
+
   // ---------------------------------------------------
   // Right Motors
   // ---------------------------------------------------
@@ -136,11 +130,17 @@ void backward(int delayMilli)
   digitalWrite(L298N_2_3A, LOW);
   digitalWrite(L298N_2_4A, HIGH);
 
-  delay(delayMilli);
+  delay(duration);
 }
 
-void spinRight(int delayMilli)
+// duration in milliseconds, speed between 0 and 255
+void spinRight(int duration, int speed)
 {
+  analogWrite(enablePin1, speed);
+  analogWrite(enablePin2, speed);
+  analogWrite(enablePin3, speed);
+  analogWrite(enablePin4, speed);
+
   // ---------------------------------------------------
   // Right Motors
   // ---------------------------------------------------
@@ -165,11 +165,17 @@ void spinRight(int delayMilli)
   digitalWrite(L298N_2_3A, LOW);
   digitalWrite(L298N_2_4A, HIGH);
 
-  delay(delayMilli);
+  delay(duration);
 }
 
-void spinLeft(int delayMilli)
+// duration in milliseconds, speed between 0 and 255
+void spinLeft(int duration, int speed)
 {
+  analogWrite(enablePin1, speed);
+  analogWrite(enablePin2, speed);
+  analogWrite(enablePin3, speed);
+  analogWrite(enablePin4, speed);
+
   // ---------------------------------------------------
   // Right Motors
   // ---------------------------------------------------
@@ -194,7 +200,44 @@ void spinLeft(int delayMilli)
   digitalWrite(L298N_2_3A, HIGH);
   digitalWrite(L298N_2_4A, LOW);
 
-  delay(delayMilli);
+  delay(duration);
+}
+
+// duration in milliseconds, speeds between 0 and 255
+// speed right > speed left = a left turn
+// speed left > speed right = a right turn
+void turn(int duration, int speedLeft, int speedRight)
+{
+  analogWrite(enablePin1, speedLeft);
+  analogWrite(enablePin2, speedLeft);
+  analogWrite(enablePin3, speedRight);
+  analogWrite(enablePin4, speedRight);
+
+  // ---------------------------------------------------
+  // Right Motors
+  // ---------------------------------------------------
+
+  // Counter Clockwise left motor
+  digitalWrite(L298N_1A, HIGH);
+  digitalWrite(L298N_2A, LOW);
+
+  // Clockwise right motor
+  digitalWrite(L298N_3A, LOW);
+  digitalWrite(L298N_4A, HIGH);
+
+  // ---------------------------------------------------
+  // Left Motors
+  // ---------------------------------------------------
+
+  // Counter Clockwise left motor
+  digitalWrite(L298N_2_1A, LOW);
+  digitalWrite(L298N_2_2A, HIGH);
+
+  // Clockwise right motor
+  digitalWrite(L298N_2_3A, HIGH);
+  digitalWrite(L298N_2_4A, LOW);
+
+  delay(duration);
 }
 
 void loop() 
